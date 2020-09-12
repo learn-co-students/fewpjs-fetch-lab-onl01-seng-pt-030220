4d4c4e1436bf0736db573954c6bfaa0d1da3325c
dylan-williamson <dylwilliamson710@gmail.com>
2020-09-12 18:02:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
+
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+}
+
+
+
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main')
@@ -10,6 +23,7 @@ function renderBooks(books) {
     main.appendChild(h2)
   })
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
