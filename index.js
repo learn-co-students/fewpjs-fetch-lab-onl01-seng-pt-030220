f30b6094d825b5cc5c873ca91ff83c8f6835278b
kelly_maass <kmaass77627@gmail.com>
2023-07-10 21:33:08 -0500

    new

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
-
 function fetchBooks() {
-return fetch('https://anapioficeandfire.com/api/books')
-.then(function(response) {
-  return response.json();
-})
-.then(function(json){
-  renderBooks(json)
-})
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    renderBooks(json)
+  })
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main')
